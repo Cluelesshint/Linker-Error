537e7f2a0d540fad948b5035e98f692376c72021
Cluelesshint <64547335+Cluelesshint@users.noreply.github.com>
2020-07-31 12:22:52 +0300

    Create my.cpp

---
+++ my.cpp
@@ -0,0 +1,16 @@
+#include "my.h"
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+inline void keep_window_open() { char ch; cin>>ch; }
+
+void print_foo(){
+    cout << foo << "\n";
+}
+
+void print(int i){
+    cout << i << "\n";
+}
